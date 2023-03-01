82b0bda75dcac1b263d4c3671a42d1f0cad5e044
Alexander_Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-03-01 20:31:52 +0300

    delete unuseful file

--- src/main/java/com/obank/config/SecurityConfig.java
+++ src/main/java/com/obank/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "home").permitAll()
+                    .antMatchers("/").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
